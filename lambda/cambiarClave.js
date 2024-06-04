02698472b1323abf569c92a50a1cc51a45c9f058
sordonez1983 <sebastian.ordz@gmail.com>
2024-06-03 23:21:25 -0500

    Actualizacion_BDD

--- lambda/cambiarClave.js
+++ lambda/cambiarClave.js
@@ -1,6 +1,29 @@
 exports.handler = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Deposito por realizar" })
+    const transaccion = JSON.parse(event.body);
+
+    const transactionDB = {
+        numeroCuenta: "123456",
+        clave: 654321,
+        estado: "completed",
+        timestamp: new Date().toISOString()
+    };
+
+    const clave = transaccion.clave;
+
+    if (transactionDB.clave != clave) {
+
+        const nuevaclave = clave;
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Cambio exitoso", nuevaclave })
+        }
+
+    }else{
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "No puede repetirse la clave" })
+        }
     }
+
 }
