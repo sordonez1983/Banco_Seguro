7cc1a6bf18e7506765c4a3b2ea68bb556bc8569f
Sebastián <jsordonez12@utpl.edu.ec>
2024-07-10 15:03:02 -0500

    Avance3

---
+++ lambda/correo.js
@@ -0,0 +1,32 @@
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
+
+exports.handler = async (event) => {
+    const params = {
+        Destination: {
+            ToAddresses: [event.to],
+        },
+        Message: {
+            Body: {
+                Text: { Data: event.body },
+            },
+            Subject: { Data: event.subject },
+        },
+        Source: 'paulnacimba@hotmail.com',
+    };
+
+    try {
+        const result = await ses.sendEmail(params).promise();
+        console.log(result);
+        return {
+            statusCode: 200,
+            body: JSON.stringify('Email enviado correctamente!'),
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify('Fallo envio de email.'),
+        };
+    }
+};
